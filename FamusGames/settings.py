376313d9936d8343e198a2abcfa6b14e03b97449
Exxotelis <109240079+Exxotelis@users.noreply.github.com>
2024-11-11 14:17:17 +0000

    VAR

--- FamusGames/settings.py
+++ FamusGames/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+rr@9fd$hj5)5az7jra(5=lx(hg^9t=b%znuxtz4-_6%#diqsh'
+SECRET_KEY = os.getenv("FAMUSGAMES_SECRET", "")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
