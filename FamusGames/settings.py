143b85a7d5cbef501b9f242cef5cc4a53f48aaa4
Lefteris Kapsalidis <109240079+Exxotelis@users.noreply.github.com>
2025-06-23 23:00:26 +0100

    Update settings.py

--- FamusGames/settings.py
+++ FamusGames/settings.py
@@ -90,12 +90,7 @@ WSGI_APPLICATION = 'FamusGames.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
     }
 }
 
